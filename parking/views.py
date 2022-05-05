3532c732f1c809e2c37b00bb9c0f876624715d8c
pookhaosc <61100378+pookhaosc@users.noreply.github.com>
2022-05-05 12:55:32 +0200

    Results page

--- parking/views.py
+++ parking/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
+from datetime import datetime
+import random
+
+
+GARAGES = [
+    'Grand Arcade',
+    'Grafton East',
+    'Grafton West',
+    'Park Street',
+    'Queen Annes Terrace',
+]
 
 
 # Create your views here.
@@ -8,3 +19,29 @@ def home(request):
     context = {}
     return render(request, 'parking/home.html', context)
 
+def results(request):
+    if request.method == 'POST':
+        destination = request.POST['destination']
+        time = request.POST['time']
+        if time == 'Now':
+            time = datetime.now().strftime("%H:%M")
+        
+
+        selected_garages = random.sample(GARAGES, 3)
+        random.shuffle(selected_garages)
+        # print(selected_garages)
+
+        # context
+        input = {
+                'destination': destination,
+                'time': time
+        }
+        output = selected_garages
+
+        context = {
+            'input': input,
+            'output': output,
+        }
+        return render(request, 'parking/results.html', context)
+    else:
+        pass

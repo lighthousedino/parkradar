b61b30f607152e7948a3f7a1b97872eb04a0bd35
pookhaosc <61100378+pookhaosc@users.noreply.github.com>
2022-05-07 09:11:06 +0200

    Rename project to ezpark

--- ezpark/settings.py
+++ ezpark/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mitigation.urls'
+ROOT_URLCONF = 'ezpark.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mitigation.wsgi.application'
+WSGI_APPLICATION = 'ezpark.wsgi.application'
 
 
 # Database
--- ezpark/wsgi.py
+++ ezpark/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mitigation.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ezpark.settings')
 
 application = get_wsgi_application()

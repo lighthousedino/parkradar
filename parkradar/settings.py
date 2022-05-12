5579e61242d996aa7faab04342c283b5b2589079
pookhaosc <61100378+pookhaosc@users.noreply.github.com>
2022-05-12 10:26:04 +0200

    Rename project to parkradar

--- parkradar/settings.py
+++ parkradar/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'ezpark.urls'
+ROOT_URLCONF = 'parkradar.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'ezpark.wsgi.application'
+WSGI_APPLICATION = 'parkradar.wsgi.application'
 
 
 # Database

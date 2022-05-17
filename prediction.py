d8e28c0b3505f6e72c319d4d7f9ee1d61ca3b6b2
pookhaosc <61100378+pookhaosc@users.noreply.github.com>
2022-05-17 15:00:34 +0200

    for luuk

--- prediction.py
+++ prediction.py
@@ -1,22 +1,57 @@
 import numpy as np
-import googlemaps
-from garages import registered_garages
-from garages.predict import predict_occupancy, simulate_sensors
-from parking.recommend import now
-from parkradar import Phoenix addressess as Pa
+import random
+import sys, os
+import csv
+import pandas as pd
 
+sys.path.append('/path/to/parkradar')
+
+from parking.recommend import now
+
+#file = open("Phoenix addressess.csv")
+#csvreader = csv.reader(file)
+#data = []
+#for row in csvreader:
+#    data.append(str(row))
+#data = np.squeeze(data)
+#data = np.squeeze(data)
+#print(data)
+data = pd.read_csv("Phoenix addressess.csv", header = None)
+#data = data.splitlines()
+#data = np.genfromtxt("Phoenix addressess.csv").splitlines()
+print(data)
 
 
 def predict(num_iters):
-  data = ()
+  days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+  time = ()
+  distance = ()
+  cure_location = ()
   for i in range(0, num_iters):
     
-    location = Pa[random.randint(0, 2700)]
+    location2 = data.iloc[random.randint(0, len(data)), 0]
+    phoenix = ", Phoenix"
+    location = location2 + phoenix
     day = random.randint(0, 6)
-    hour = random.randint(0, 24)
+    day = days[day]
+    time = str(random.randint(0, 24))
+    time2 = ":00"
+    hour = time + time2
+    print(day, hour, location)
     
-    data.append(now(location, day, hour))
+    recommendation = now(location, day, hour)
     
-    return data
-  
+    cure_location.append(location2)
+    time.append(recommendation["estimated_distance"])
+    distance.append(recommendation["estimated_time"])
+    
+  #data = np.column_stack(cure_location, time)
+  #data = np.column_stack(data, distance)
+    
+  return cure_location, time, distance
+
+cure_location, time, distance = predict(1000)
+np.savetxtx("Model_data.csv", (cure_location, time, distance), delimiter = ',')
+
+
 
